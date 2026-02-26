e2f7be607a9a05eb4cb7fadd557c9de04df76758
Luis Eduardo Orjuela Rojas <luchorjuela@hotmail.com>
2026-02-26 13:55:48 -0500

    First project uploading

---
+++ Assets/Scripts/ArrivalProcess.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrivalProcess : MonoBehaviour
+{
+
+    public GameObject carPrefab;
+    public Transform carSpawnPlace;
+
+    [Header("Constant, or Exponential Distribution" )]
+    public float arrivalRateAsCarsPerHour = 20; // car/hour
+    public float interArrivalTimeInHours; // = 1.0 / arrivalRateAsCarsPerHour;
+    private float interArrivalTimeInMinutes;
+    private float interArrivalTimeInSeconds;
+
+
+    //public float arrivalRateAsCarsPerHour = 20; // car/hour
+    public bool generateArrivals = true;
+
+    //New as of Feb.23rd
+    //Simple generation distribution - Uniform(min,max)
+    //
+    [Header("Uniform Distribution")]
+    public float minInterArrivalTimeInSeconds = 3; 
+    public float maxInterArrivalTimeInSeconds = 60;
+
+    [Header("Triangular Distribution")]
+    //Ref: https://en.wikipedia.org/wiki/Triangular_distribution
+    public float a=3, b=7, c=5; // You should have c in (a,b)   a<c<b
+    //
+    [Header("Observed Distribution")]
+    [Tooltip("These are Cumulative Distribution data ys")]
+    public float[] xs = { 0, 1, 2, 10 };
+    [Tooltip("These are Cumulative Distribution data xs")]
+    public float[] ys = { 0, .75f, .97f, 1f };
+   public enum ArrivalIntervalTimeStrategy
+    {
+        ConstantIntervalTime,
+        UniformIntervalTime,
+        ExponentialIntervalTime,
+        ObservedIntervalTime,
+        TriangularDistribution
+    }
+
+    public ArrivalIntervalTimeStrategy arrivalIntervalTimeStrategy=ArrivalIntervalTimeStrategy.UniformIntervalTime;
+
+    //New as of Feb.25th
+    QueueManager queueManager;
+
+    //UI debugging
+#if DEBUG_AP
+    public Text txtDebug;
+#endif
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        queueManager = GameObject.FindGameObjectWithTag("DriveThruWindow").GetComponent<QueueManager>();
+        interArrivalTimeInHours = 1.0f / arrivalRateAsCarsPerHour;
+        interArrivalTimeInMinutes = interArrivalTimeInHours * 60;
+        interArrivalTimeInSeconds = interArrivalTimeInMinutes * 60;
+        StartCoroutine(GenerateArrivals());
+#if DEBUG_AP
+        print("proc#:" + System.Environment.ProcessorCount);
+        txtDebug.text = "\nproc#:" + System.Environment.ProcessorCount;
+#endif
+    }
+   
+    IEnumerator GenerateArrivals()
+    {
+        while (generateArrivals)
+        {
+            GameObject carGO=Instantiate(carPrefab, carSpawnPlace.position, Quaternion.identity);
+            //if (queueManager.Count() > 0)
+            //{
+            //    queueManager.Add(carGO);
+            //} //The first car as added in the queue when in DriveThruWindow
+
+            float timeToNextArrivalInSec = interArrivalTimeInSeconds;
+            float U = Random.value;
+            switch (arrivalIntervalTimeStrategy)
+            {
+                case ArrivalIntervalTimeStrategy.ConstantIntervalTime:
+                    timeToNextArrivalInSec= interArrivalTimeInSeconds;
+                    break;
+                case ArrivalIntervalTimeStrategy.UniformIntervalTime:
+                    timeToNextArrivalInSec = Random.Range(minInterArrivalTimeInSeconds, maxInterArrivalTimeInSeconds);
+                    break;
+                case ArrivalIntervalTimeStrategy.ExponentialIntervalTime:
+                    
+                    float Lambda = 1 / arrivalRateAsCarsPerHour;
+                    timeToNextArrivalInSec = Utilities.GetExp(U,Lambda);
+                    break;
+                case ArrivalIntervalTimeStrategy.ObservedIntervalTime:
+                    timeToNextArrivalInSec = Utilities.MultiInterpolate(ys,xs,U) * 60; //we get it in min, so *60 => in sec
+                    break;
+                case ArrivalIntervalTimeStrategy.TriangularDistribution:
+                    timeToNextArrivalInSec = Utilities.GetTriangularDistribution(U, a,b,c);
+                    break;
+                default:
+                    print("No acceptable arrivalIntervalTimeStrategy:" + arrivalIntervalTimeStrategy);
+                    break;
+
+            }
+
+            //New as of Feb.23rd
+            //float timeToNextArrivalInSec = Random.Range(minInterArrivalTimeInSeconds,maxInterArrivalTimeInSeconds);
+            yield return new WaitForSeconds(timeToNextArrivalInSec);
+
+            //yield return new WaitForSeconds(interArrivalTimeInSeconds);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+internal class Utilities
+{
+    internal static float GetExp(float u, float lambda)
+    {
+        //throw new NotImplementedException();
+        return -Mathf.Log(1 - u) / lambda;
+    }
+
+    internal static float GetTriangularDistribution(float u, float a, float b, float c)
+    {
+        //Ref: https://en.wikipedia.org/wiki/Triangular_distribution
+        //public float a = 3, b = 7, c = 5; // You should have c in (a,b)   a<c<b
+
+        //throw new NotImplementedException();
+        float V = (c - a) / (b - a);
+        //print("V=" + V);
+        float res = 0f;
+        if (u < V)
+        {
+            res = a + Mathf.Sqrt(u * (b - a) * (c - a));
+        }
+        else if (u>= V)
+        {
+            res = b - Mathf.Sqrt((1-u)* (b - a) * (b- c));
+        } 
+        else{
+            //print("Wrong U, or a or b or c; u is in [0,1), c is in (a,b)");
+            Console.WriteLine("Wrong U, or a or b or c; u is in [0,1), c is in (a,b)");
+        }
+
+
+
+        return res;
+    }
+
+
+    //LinearRegression
+    //xs[i], ys[i], i in [0,N-1]
+    //y=A*x+B   , ys[i]=A*xs[i]+B + e_i
+    // when r close to 1, almost perfect increasing line fit
+    // when r close to -1, almost perfect decreasing line fit
+    // when r close to 0, no correlation
+
+    public static void LinearRegression(float[] xs, float[] ys, out float A, out float B, out float r_xy, out float MSE)
+    {
+        float sx = 0, sy = 0, sxx = 0, sxy = 0, syy = 0;
+        int n = xs.Length;
+        for (int i = 0; i < n; i++)
+        {
+            sx += xs[i];
+            sy += ys[i];
+            sxy += xs[i] * ys[i];
+            sxx += xs[i] * xs[i];
+            syy += ys[i] * ys[i];
+        }
+
+        A = (n * sxy - sx * sy) / (n * sxx - sx * sx);
+        B = (sy - A * sx) / n;
+        r_xy = (n * sxy - sx * sy) / Mathf.Sqrt((n * sxx - sx * sx) * (n * syy - sy * sy));
+        MSE = 0;
+        for (int i = 0; i < n; i++)
+        {
+            float e_i = (ys[i] - A * xs[i] - B);
+            MSE += e_i * e_i;
+        }
+        MSE /= n;
+    }
+    //Euler Method
+    public static void EulerMethod(float x_i, float y_i, float step, Func<float, float, float> f, out float new_x, out float new_y)
+    {
+        new_x = x_i + step;
+        new_y = y_i + step * f(x_i, y_i); //Euler Scheme
+
+    }
+
+    //RK4
+    public static void RK4(float x_i, float y_i, float step, Func<float, float, float> f, out float new_x, out float new_y)
+    {
+        float x1 = x_i;
+        float x2 = x_i + step / 2;
+        float x3 = x_i + step / 2;
+        float x4 = x_i + step;
+
+        float y1, y2, y3, y4, K1, K2, K3, K4, K;
+        y1 = y_i;
+        K1 = f(x1, y1);
+
+        y2 = y_i + step / 2 * K1;
+        K2 = f(x2, y2);
+
+        y3 = y_i + step / 2 * K2;
+        K3 = f(x3, y3);
+
+        y4 = y_i + step * K3;
+        K4 = f(x4, y4);
+
+        K = (K1 + 2 * K2 + 2 * K3 + K4) / 6f;
+
+        new_x = x_i + step;
+        new_y = y_i + step * K; //RK4 Scheme
+
+    }
+    //MultiInterpolation
+    public static float MultiInterpolate(float[] xs, float[] ys, float x)
+    {
+        float y = 0;
+        for (int i = 0; i < xs.Length - 1; i++)
+        {
+            //guard in case we have a flat segment in the cumulative distribution
+            if (Mathf.Abs(xs[i + 1] - xs[i]) <= float.Epsilon)
+            {
+                //return ys[i];//
+                //return ys[i+1];//
+                return (ys[i] + ys[i + 1]) / 2;
+
+            }
+            if (x >= xs[i] && x < xs[i + 1])
+            {
+                return Mathf.Lerp(ys[i], ys[i + 1], x / (xs[i + 1] - xs[i]));
+            }
+
+        }
+        return y;
+    }
+
+}
